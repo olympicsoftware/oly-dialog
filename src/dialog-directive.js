0374437b5905ff40f498d6ab1816773baa89a4fd
Andrew Hughson <andrewh@olympic.co.nz>
2015-03-18 23:09:31 +1300

    Check if the dialog is open before trying to close it when the directive is destroyed.

--- src/dialog-directive.js
+++ src/dialog-directive.js
@@ -52,7 +52,9 @@ export default function dialog() {
             };
 
             $scope.$on('$destroy', () => {
-                this.close();
+                if (this.isOpen()) {
+                    this.close();
+                }
 
                 dialogRegistry.removeDialog($attrs.name);
             });
